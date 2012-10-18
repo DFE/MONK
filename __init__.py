e2537d39cac9e2348920862ef67e87c0785ec271
Thilo Fromm <github@thilo-fromm.de>
2012-10-18 10:44:24 +0200

    __init__: created, public API added
    This patch adds __init__.py to G.O.R.D.O.N., thereby making it a full
    blown Python module. Also, all classes which are supposed to be used
    publicly are imported by __init__.py so they can be imported a la
    
    >>>
    >>> from Gordon import Device
    >>>
    
    Signed-off-by: Thilo Fromm <github@thilo-fromm.de>
    Acked-by: Steffen Sledz <sledz@dresearch-fe.de>

---
+++ __init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python -tt
+# -*- coding: utf-8 -*-
+#
+# HidaV automated test framework module API
+#
+# Copyright (C) 2012 DResearch Fahrzeugelektronik GmbH
+# Written and maintained by Thilo Fromm <fromm@dresearch-fe.de>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version
+# 2 of the License, or (at your option) any later version.
+#
+
+
+
+# G.O.R.D.O.N. automated integration test framework public API
+
+from devicetestcase import DeviceTestCase
+from device         import Device
+from connection     import Connection
+from ssh_conn       import SshConn
+from serial_conn    import SerialConn
+from bcc            import Bcc
+
