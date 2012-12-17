e5d3cc2088585d073db4abca4fc35f0e9cb56128
Erik Bernoth <bernoth@dresearch-fe.de>
2012-12-17 14:48:54 +0100

    Comments renamed
    The comments still contained a note to the HidaV project, which had to be
    removed.
    
    A change for issue #8.
    
    Signed-off-by: Erik Bernoth <bernoth@dresearch-fe.de>

--- tests/test_connection.py
+++ tests/test_connection.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
 #
-# HidaV automated test framework connection class unit tests
+# GORDON Automated Testing Framework
 #
 # Copyright (C) 2012 DResearch Fahrzeugelektronik GmbH
 # Written and maintained by Thilo Fromm <fromm@dresearch-fe.de>
