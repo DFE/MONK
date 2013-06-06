116c21ee9762b1140afb2167191b4b93961acc05
Erik Bernoth <erik.bernoth@gmail.com>
2013-06-06 16:38:15 +0200

    LICENSE: Add GPLv3+
    Now all source files should follow GPLv3 source licensing.
    
    See GitHub Issue #65.
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>
    Acked-by: Eik Binschek <binschek@dresearch-fe.de>

--- monk_tf/ssh_conn.py
+++ monk_tf/ssh_conn.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-# MONK automated test framework ssh connection handling
+# Copyright (C) 2012, 2013 DResearch Fahrzeugelektronik GmbH
+# Maintained by project-monk@dresearch-fe.de
 #
-# Copyright (C) 2012 DResearch Fahrzeugelektronik GmbH
-# Written and maintained by Thilo Fromm <fromm@dresearch-fe.de>
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version
-# 2 of the License, or (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 
 """ Package for the SSH / SCP Connection class """
