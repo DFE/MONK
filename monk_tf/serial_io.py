ce2a966568eaf17ff5a6f865a05b9e1c83131472
Erik Bernoth <erik.bernoth@gmail.com>
2013-06-18 10:56:06 +0200

    serial_io: fundamental unit tests and debugging
    Added tests for Object creation, a basic cmd() and 2 cases for the read_until
    methods. The last method needed an additonal unit test, because start_strip and
    end_strip should be tested separately.
    
    See GitHub Issue #60.
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- monk_tf/serial_io.py
+++ monk_tf/serial_io.py
@@ -17,7 +17,7 @@ import logging
 
 import serial
 
-class SerialConsole(serial.Serial):
+class SerialIO(serial.Serial):
     """ console like interface on top of pyserial
 
     The job of this class is to allow a console like input-output behaviour
@@ -36,13 +36,14 @@ class SerialConsole(serial.Serial):
         :param prompt: set a prompt for the communication
         """
         self._logger = logging.getLogger(__name__)
-        self.prompt = Serial._DEFAULT_PROMPT
-        if args:
-            self.prompt = prompt
-        super(Serial,self).__init__(*args, **kwargs)
+        self.prompt = SerialIO._DEFAULT_PROMPT
+        if "prompt" in args:
+            self.prompt = args["prompt"]
+            args.pop("prompt")
+        super(SerialIO,self).__init__(*args, **kwargs)
 
 
-    def cmd(self, msg, prompt=None, sleep_length=.1, timeout=5, linesep="\n"):
+    def cmd(self, msg, prompt=None, sleep_time=.1, timeout=5, linesep="\n"):
         """ send a command and retrieve it's response.
 
         Text that might be in the buffer before the command is ignored, as well
@@ -53,7 +54,7 @@ class SerialConsole(serial.Serial):
         The basic structure of a command is like that::
 
             <something left in the buffer><our command><EOL>
-            <command output><EOL> # optional (EOLs inside the command output are treated as any character)
+            <command output> # optional (EOLs inside the command output are treated as any character)
             <prompt><space>
 
         We try to just grab the ``<command output>`` and return it.
@@ -68,7 +69,7 @@ class SerialConsole(serial.Serial):
                        default is used and if that is also not set the default
                        python prompt is used: ``>>> ``.
 
-        :param sleep_length: defines how long the process should sleep until
+        :param sleep_time: defines how long the process should sleep until
                              the next iteration of the loop is started.
 
         :param timeout: defines in seconds how long this method should take at
@@ -79,20 +80,25 @@ class SerialConsole(serial.Serial):
 
         :param return: the command output.
         """
-        # prepare
+        prompt = prompt if prompt else self.prompt
         cmd = msg.strip() + linesep
         self.write(cmd)
-        self.__last_cmd = cmd
-        self.__last_confidence, self.__last_output =  self.read_until(
-                cmd,prompt, sleep_length, timeout)
+        self._last_cmd = cmd
+        self._logger.debug(str((self, cmd, prompt, sleep_time, timeout)))
+        self._last_confidence, self._last_output =  self.read_until(
+                prompt,
+                cmd,
+                sleep_time,
+                timeout
+        )
         return self.last_output
 
 
-    def read_until(self, end_strip, start_strip=None, sleep_length=.1, timeout=5):
+    def read_until(self, end_strip, start_strip=None, sleep_time=.1, timeout=5):
         """ read until end strip found
 
         This function reads everything available in the buffer, then waits
-        ``sleep_length`` seconds and then starts again, either until it finds
+        ``sleep_time`` seconds and then starts again, either until it finds
         ``end_strip`` in the buffered text or the ``timeout`` runs out.
         Everything will be deleted, beginning from the ``end_strip``, because
         that is expected to be knowledge the user already has.
@@ -101,13 +107,19 @@ class SerialConsole(serial.Serial):
         ``start_strip``, which will delete everything in the buffer until the
         start_strip is found.
 
+        All of that is a generalization of how the method is used in cmd(). The
+        idea is that a command line execution over serial repeats the written
+        command, then writes the command output and finally prints a new
+        prompt. Therefore it makes sense to look for the command and the prompt
+        as the delimiters of the desired part of the output.
+
         :param end_strip: The text which terminates the search. A string as
         excepted by :py:func:`str.find`.
 
         :param start_strip: The text which starts the search. A string as
         expected by :py:func:`str.find`.
 
-        :param sleep_length: defines how long the process should sleep until
+        :param sleep_time: defines how long the process should sleep until
                              the next iteration of the loop is started.
 
         :param timeout: defines in seconds how long this method should take at
@@ -117,24 +129,23 @@ class SerialConsole(serial.Serial):
                  timeout didn't deplete. The second param contains the output
                  as far as it was received.
         """
-        state = ReadState.LEFT_OVER if start_strip else ReadState.FOUND_START
+        state = SerialIO.ReadState.LEFT_OVER if start_strip else SerialIO.ReadState.FOUND_START
         out = ""
         start_time = time.time()
         while True:
             if time.time() - start_time >= timeout:
-                self.__last_confidence = False
-                self.__last_cmd = msg
-                self.__last_output = out
-                return out
-            time.sleep(sleep_length)
+                return False, out
+            # this should be also executed before first loop
+            # because this function is also used by write
+            time.sleep(sleep_time)
             out += self.read(self.inWaiting())
-            if state == ReadState.LEFT_OVER:
+            if state == SerialIO.ReadState.LEFT_OVER:
                 pos = out.find(start_strip)
                 if pos >= 0:
                     # forget everything up to including the start_strip
                     out = out[pos+len(start_strip):]
-                    state = ReadState.FOUND_START
-            elif state == ReadState.FOUND_START:
+                    state = SerialIO.ReadState.FOUND_START
+            elif state == SerialIO.ReadState.FOUND_START:
                 pos = out.find(end_strip)
                 if pos >= 0:
                     # strip the end_strip and everything afterwards
@@ -146,20 +157,20 @@ class SerialConsole(serial.Serial):
     @property
     def prompt(self):
         try:
-            return self.__prompt
+            return self._prompt
         except AttributeError:
             return None
 
 
     @prompt.setter
     def prompt(self,new_prompt):
-        self.__prompt = new_prompt
+        self._prompt = new_prompt
 
 
     @property
     def last_confidence(self):
         try:
-            return self.__last_confidence
+            return self._last_confidence
         except AttributeError:
             return None
 
@@ -167,7 +178,7 @@ class SerialConsole(serial.Serial):
     @property
     def last_cmd(self):
         try:
-            return self.__last_cmd
+            return self._last_cmd
         except AttributeError:
             return None
 
@@ -175,6 +186,6 @@ class SerialConsole(serial.Serial):
     @property
     def last_output(self):
         try:
-            return self.__last_output
+            return self._last_output
         except AttributeError:
             return None
