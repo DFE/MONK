e71cf6760507e95e3874aa4a46c6c2dd9c78bad6
Erik Bernoth <erik.bernoth@gmail.com>
2014-01-22 16:08:21 +0100

    setup.py: Update for more verbose PyPi page
    The PyPi Page of v0.1.3 was really too bad for public presentation and because
    I want to present a good thesis, I want to make sure that this page gets
    improved.
    
    See GitHub Issue #85.
    
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>
    Acked-by: Eik Binschek <binschek@dresearch-fe.de>

--- setup.py
+++ setup.py
@@ -11,24 +11,56 @@
 # 2 of the License, or (at your option) any later version.
 #
 
-from setuptools import setup
+import os
+import sys
 
-project = "monk_tf"
-version = "0.1.4"
+import monk_tf
+
+try:
+    from setuptools import setup
+except ImportError as error:
+    from distutils.core import setup
+
+if sys.argv[-1] == 'publish':
+    sys.argv = [sys.argv[0], 'sdist', 'upload']
+
+
+def read(name):
+    with open(name, 'r') as f:
+        return f.read()
 
 setup(
-    name=project,
-    version=version,
-    description = "a test framework for embedded systems",
-    author = "DResearch Fahrzeugelektronik GmbH",
+    name=monk_tf.__title__,
+    version=monk_tf.__version__,
+    description = "regression test scripting framework for embedded systems developers",
+    long_description = read("README.rst") + "\n\n" + read("CHANGES.rst"),
+    author = monk_tf.__author__,
     author_email = "project-monk@dresearch-fe.de",
     url="https://github.com/DFE/MONK",
-    packages=[project],
+    packages=[monk_tf.__title__],
+    license=read("LICENSE.txt"),
+    zip_safe=False,
     install_requires = [
         "pyserial >=2.5",
         "configobj >=4.7.2",
     ],provides = [
-        "{} ({})".format(project, version)
+        "{} ({})".format(monk_tf.__title__, monk_tf.__version__)
     ],
-    test_suite = "nose.collector"
+    test_suite = "nose.collector",
+    classifiers=(
+        "Development Status :: 3 - Alpha",
+        "Environment :: Other Environment",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Telecommunications Industry",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Natural Language :: English",
+        "Operating System :: Unix",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Terminals :: Serial",
+    ),
 )
