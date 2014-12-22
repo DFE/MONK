c61a81c44dbd4c9d029b4b0872398865f6d087e0
Erik Bernoth <erik.bernoth@gmail.com>
2014-12-22 17:39:52 +0100

    WIP: tests
    Signed-off-by: Erik Bernoth <erik.bernoth@gmail.com>

--- test/test_conn.py
+++ test/test_conn.py
@@ -17,22 +17,67 @@ from monk_tf import conn
 def test_simplest():
     """ conn: create the simplest possible AConnection
     """
-    # nothing to prepare
     # execute
     sut = conn.ConnectionBase('')
-    # assert
-    nt.ok_(sut, "should contain an AConnection object, but contains '{}'"
-            .format(sut))
+    # verify
+    nt.ok_(sut)
 
-def test_cmd_returncode():
-    """ conn: test connections can handle additional parameters
+def test_send_echo():
+    """ conn: send echo 123 and receive 123
     """
-    # set up
-    sut = conn.EchoConnection()
-    sut2 = conn.DefectiveConnection()
-    # execute + assert (raises Error if params can't be parsed)
-    sut._cmd("hello", returncode=True)
-    try:
-        out = sut2._cmd("hello", returncode=True)
-    except conn.MockConnectionException as e:
-        pass
+    # setup
+    sut = MockConn(name='', out="123", retcode="<retcode>0</retcode>")
+    expected_retcode = 0
+    expected_out = "123"
+    # execute
+    retcode, out = sut.cmd("echo 123")
+    # verify
+    nt.eq_(retcode, expected_retcode)
+    nt.eq_(out, expected_out)
+    raise Exception(sut._calls)
+
+def test_ignore_retcode():
+    """ conn: disable retcode via argument
+    """
+    # setup
+    sut = MockConn('')
+    expected_retcode = 0
+    expected_out = "123"
+    # execute
+    retcode, out = sut.cmd("echo 123", do_retcode=False)
+    # verify
+    nt.eq_(retcode, expected_retcode)
+    nt.eq_(out, expected_out)
+
+# did it close properly?
+# does it recover?
+
+class MockConn(conn.ConnectionBase):
+
+    def __init__(self, *args, **kwargs):
+        self._exp = Exp()
+        self._exp.before = kwargs.pop("out", None)
+        self.retcode = kwargs.pop("retcode", "")
+        self._exp.after = self.retcode
+        super(MockConn, self).__init__(*args, **kwargs)
+        self._calls = {}
+        self.prompt = "asdf"
+
+    def _call_method(self, name, args):
+        self._calls[name] = args
+
+    def _expect(self, pattern, timeout=-1, searchwindowsize=-1):
+        self._call_method("_expect", (pattern, timeout, searchwindowsize))
+
+    def _send(self, pattern, timeout=-1, searchwindowsize=-1):
+        self._call_method("_send", (pattern, timeout, searchwindowsize))
+
+    def _sendline(self, pattern, timeout=-1, searchwindowsize=-1):
+        self._call_method("_sendline", (pattern, timeout, searchwindowsize))
+
+    @property
+    def exp(self):
+        return self._exp
+
+class Exp(object):
+    pass
